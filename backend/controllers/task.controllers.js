3d130bcb5408ef028847e36d2b1cdc8747fa13d0
rohit negi <rohitnegi263139@gmail.com>
2025-04-03 12:47:41 +0530

    create api for update and delete task

--- backend/controllers/task.controllers.js
+++ backend/controllers/task.controllers.js
@@ -38,3 +38,43 @@ export const getTasks = async (req, res) => {
 		res.status(400).send(error.message);
 	}
 };
+
+export const editTask = async (req, res) => {
+	try {
+		const userId = req.user._id;
+		const { taskId } = req.params;
+		const user = await User.findById(userId);
+		if (!user) {
+			throw new Error("User not found!");
+		}
+		const { task } = req.body;
+		const updatedTask = await Task.findByIdAndUpdate(taskId, { task });
+		await updatedTask.save();
+		if (!updatedTask) {
+			throw new Error("Something went Wrong. Please try again");
+		} else {
+			res.status(200).json({ message: "Task Updated", data: updatedTask });
+		}
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+};
+
+export const removeTask = async (req, res) => {
+	try {
+		const userId = req.user._id;
+		const { taskId } = req.params;
+		const user = await User.findById(userId);
+		if (!user) {
+			throw new Error("User not found!");
+		}
+		const removeTask = await Task.findByIdAndDelete(taskId);
+		if (!removeTask) {
+			throw new Error("Something went Wrong. Please try again");
+		} else {
+			res.status(200).json({ message: "Task Deleted" });
+		}
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+};
--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { createTask, getTasks } from "../controllers/task.controllers.js";
+import {
+	createTask,
+	editTask,
+	getTasks,
+	removeTask,
+} from "../controllers/task.controllers.js";
 import verifyToken from "../utils/verifyToken.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, createTask);
 router.get("/all", verifyToken, getTasks);
+router.patch("/edit/:taskId", verifyToken, editTask);
+router.delete("/remove/:taskId", verifyToken, removeTask);
 export default router;
